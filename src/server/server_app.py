0e6ae1b0949f0e6718157728639a08155e449858
Zach Rosenberg <zacharyrrosenberg@gmail.com>
2021-01-26 10:46:30 -0500

    very simple api example

--- src/server/server_app.py
+++ src/server/server_app.py
@@ -1,28 +1,77 @@
 import json
 import flask
+from flask import request
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+# This is a very simple web server!
+# Each website page is called a "route". The functions on the server
+# get called when you go to the page listed on the route.
+
+# You can get data or send data to the server with the different http GET/POST
+# commands. This server does not show any html pages, so it responds to manual
+# http requests. These can be made using the python "requests" library.
+
+
+# This server isn't setup to use the actuall bookshelf code yet, it's just a
+# basic stand-in or framework for us to fill in.
 
 @app.route('/', methods=['GET'])
 def home():
-        return (
-            "<h1>Distant Reading Archive</h1><p>This site is a prototype "
-            "API for distant reading of science fiction novels.</p>"
-        )
+    return (
+        "<h1>BOOKSHELF</h1><p> Your own, distributed library!</p>"
+    )
+
+
 @app.route('/api/zach', methods=['GET'])
 def api_zach():
-        return (
-            "zach's info"
-        )
+    return (
+        "zach's info"
+    )
 
 
 @app.route('/login', methods=['POST'])
 def login():
-    user = request.payload
+    user = request.form["user"]
     print(user)
     return user
 
 
+@app.route('/register', methods=['POST'])
+def register():
+    # Add a new user to the user_data dictionary
+
+    user = request.form['user']
+    passwd = request.form['password']
+
+    if user in user_data:
+        return "User already exists"
+    user_data[user] = {
+        "password": passwd,
+        "book_list": [],
+        "friends": []  # list of friends
+    }
+    print("User added: ", user)
+    return f"User {user} added!"
+
+
+@app.route('/books/<user>', methods=['GET', 'POST'])
+def books(user):
+    # This function can be called with a get or post.
+    # GET returns the list of books for the given user.
+
+    if request.method == 'GET':
+        return json.dumps(user_data[user]["book_list"])
+    if request.method == 'POST':
+        book_to_add = request.form['book']
+        user_data[user]["book_list"].append(book_to_add)
+        return "book added"
+
+
+# Dict of users, with password and bookshelf info
+user_data = {}
+
+# This starts the server. The other functions are called when a request is
+# recieved.
 app.run()
